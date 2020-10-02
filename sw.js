7841025623bc408dfd57e7452d4461e0e7a4f97f
shouvikbasu <babanomania@gmail.com>
2020-10-02 22:33:31 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8ce1a967a91f7da3f6ee.js"
+    "url": "webpack-runtime-44d8d2790d1e91049661.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "544f43b096a57ebd728e81d4b3a63da1"
+    "revision": "afcbafb0eb98a779ce8627f020e09ee0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
